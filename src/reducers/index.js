c9706d2b41a9c709507b448dc364889c304ebbd9
Dionne Stratton <mylife33@hotmail.com>
2020-11-05 21:43:57 -0600

    na

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,11 @@ export const reducer = (state, action) => {
   switch (action.type) {
     case GET_CANVAS:
       return { ...state, name: action.payload };
-    case GET_POSTER:
+    case GET_POSTERP:
       return { ...state, location: action.payload };
     default:
       return state;
   }
 };
 const GET_CANVAS = "GET_CANVAS";
-const GET_POSTER = "GET_POSTER";
+const GET_POSTERP = "GET_POSTER";
