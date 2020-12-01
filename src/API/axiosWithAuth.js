79992ef8c39c767d39863def20d61973e61bcbe1
Dionne Stratton <mylife33@hotmail.com>
2020-11-30 20:57:28 -0600

    shop fix

--- src/API/axiosWithAuth.js
+++ src/API/axiosWithAuth.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-export const axiosWithAuthGallery = () => {
-  const token = "kh95KdcSSJAuLQr_1hXKF1ANzkro4HGGivozprOwOvk";
-  const spaceId = "3j13juwb67cs";
-  const environmentId = "master";
+// export const axiosWithAuthGallery = () => {
+//   const token = "kh95KdcSSJAuLQr_1hXKF1ANzkro4HGGivozprOwOvk";
+//   const spaceId = "3j13juwb67cs";
+//   const environmentId = "master";
 
-  return axios.create({
-    headers: {
-      Authorization: token,
-      space: spaceId,
-      environment: environmentId,
-    },
-    baseURL: "https://cdn.contentful.com",
-  });
-};
+//   return axios.create({
+//     headers: {
+//       Authorization: token,
+//       space: spaceId,
+//       environment: environmentId,
+//     },
+//     baseURL: "https://cdn.contentful.com",
+//   });
+// };
 
 export const axiosWithAuth = () => {
   const token =
