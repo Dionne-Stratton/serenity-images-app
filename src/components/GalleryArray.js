7fc3dad5330ea3f98f5e1c9b6868b538cdbce611
Dionne <mylife33@hotmail.com>
2021-02-18 00:53:05 -0600

    gallery overhaul

--- src/components/GalleryArray.js
+++ src/components/GalleryArray.js
@@ -1,18 +1,16 @@
-import swren from "../images/galleryPics/splendid wren.jpg";
-import sunP from "../images/galleryPics/sun parakeet.jpg";
-import redrumpd from "../images/galleryPics/redrumpd.jpg";
-import acaia from "../images/galleryPics/acaiatree.jpg";
-import cannonballflwrs from "../images/galleryPics/cannonballflwrs.jpg";
-import celestial from "../images/galleryPics/celestialGate.jpg";
-import cherryb from "../images/galleryPics/cherry blossoms.jpg";
-import palmBeach from "../images/galleryPics/beach.jpg";
-import fairyGate from "../images/galleryPics/fairy gate.jpg";
-import blueM from "../images/galleryPics/blue macaw.jpg";
-import joy from "../images/galleryPics/joy.jpg";
-import mags from "../images/galleryPics/magnolias.jpg";
-import owlA from "../images/galleryPics/owl.jpg";
-import kingF from "../images/galleryPics/king fisher.jpg";
-import lunaM from "../images/galleryPics/luna moth.jpg";
+import swren from "../images/galleryPics/sunset-fairy-wren.jpg";
+import sunP from "../images/galleryPics/sunParakeet.jpg";
+import redrumpd from "../images/galleryPics/redRumped.jpg";
+import cannonballflwrs from "../images/galleryPics/cannonballflwrs2.jpg";
+import celestial from "../images/galleryPics/celestialGate2.jpg";
+import cherryb from "../images/galleryPics/cherryBlossoms.jpg";
+import fairyGate from "../images/galleryPics/fairyGate.jpg";
+import blueM from "../images/galleryPics/blueMacaw2.jpg";
+import mags from "../images/galleryPics/magnolias2.jpg";
+import nightOwl from "../images/galleryPics/nightOwl.jpg";
+import kingF from "../images/galleryPics/kingfisherSunset.jpg";
+import lunaM from "../images/galleryPics/lunaMoth.jpg";
+import celestialB from "../images/galleryPics/bigger/celestial-gate-b.jpg";
 
 
 const GalleryArray = [
@@ -64,17 +62,17 @@ const GalleryArray = [
     page:
     "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/cannonballflwrs.jpg",
   },
-  {
-    title: "Acaia Tree",
-    id: 5,
-    category: "landscape tree acaia",
-    medium: "canvas sheet acrylic",
-    baseImage: acaia,
-    sizes: "small 9 x 12",
-    description: "Acrylic paint on 9x12 inch canvas sheet.",
-    price: 50,
-    page:"https://user-interface-ii-5fl9n5s96.vercel.app/images/art/acaia%20tree.jpg",
-  },
+  // {
+  //   title: "Acaia Tree",
+  //   id: 5,
+  //   category: "landscape tree acaia",
+  //   medium: "canvas sheet acrylic",
+  //   baseImage: acaia,
+  //   sizes: "small 9 x 12",
+  //   description: "Acrylic paint on 9x12 inch canvas sheet.",
+  //   price: 50,
+  //   page:"https://user-interface-ii-5fl9n5s96.vercel.app/images/art/acaia%20tree.jpg",
+  // },
   {
     title: "Celestial Gate",
     id: 7,
@@ -84,7 +82,7 @@ const GalleryArray = [
     size: "small 11 x 14",
     description: "Acrylic paint on 11x14 inch wrapped canvas.",
     price: 70,
-    page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/celestial%20gate.jpg",
+    page: celestialB,
   },
   {
     title: "Cherry Blossoms",
@@ -97,17 +95,17 @@ const GalleryArray = [
     price: 50,
     page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/cherry%20blossoms.jpg",
   },
-  {
-    title: "Palm Tree Beach",
-    id: 8,
-    category: "ocean beach seascape palmtree palm tree",
-    medium: "canvas wrap acrylic",
-    baseImage: palmBeach,
-    size: "medium 16 x 20",
-    description: "Acrylic paint on 16x20 inch wrapped canvas.",
-    price: 200,
-    page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/beach.jpg",
-  },
+  // {
+  //   title: "Palm Tree Beach",
+  //   id: 8,
+  //   category: "ocean beach seascape palmtree palm tree",
+  //   medium: "canvas wrap acrylic",
+  //   baseImage: palmBeach,
+  //   size: "medium 16 x 20",
+  //   description: "Acrylic paint on 16x20 inch wrapped canvas.",
+  //   price: 200,
+  //   page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/beach.jpg",
+  // },
   {
     title: "Fairy Gate",
     id: 6,
@@ -117,7 +115,7 @@ const GalleryArray = [
     size: "small 9 x 12",
     description: "Acrylic paint on 9x12 inch canvas paper.",
     price: 50,
-    page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/fairy%20gate.jpg",
+    page: "https://ctl.s6img.com/society6/img/bTDxG4Bq3K-6uyb0mGe4rwCv3aM/w_700/canvas/~artwork/s6-original-art-uploads/society6/uploads/misc/8c40c2ff7a904bfabd83c46436536170/~~/fairy-gate2730911-canvas.jpg?wait=0&attempt=0",
   },
   {
     title: "Blue Macaw",
@@ -131,15 +129,15 @@ const GalleryArray = [
     page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/blue%20macaw.jpg",
   },
   {
-    title: "Owl on Cherry Tree",
+    title: "Night Owl",
     id: 11,
     category: "birds flowers night owl cherry tree blossoms",
     medium: "canvas sheet acrylic",
-    baseImage: owlA,
+    baseImage: nightOwl,
     size: "small 9 x 12",
     description: "Acrylic paint on 9x12 inch canvas paper.",
     price: 50,
-    page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/owl.jpg",
+    // page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/owl.jpg",
   },
   {
     title: "Luna Moth",
@@ -161,19 +159,19 @@ const GalleryArray = [
     size: "medium 11 x 14 ",
     description: "Acrylic paint on 11x14 inch wrapped canvas.",
     price: 70,
-    page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/king%20fisher.jpg",
+    page: "https://ctl.s6img.com/society6/img/ErFGAIQeZfdB4xLeE3e8J_wtKBc/w_700/canvas/~artwork/s6-original-art-uploads/society6/uploads/misc/30a08495e88842ce9bf8e97c5136309f/~~/kingfisher-sunset2724780-canvas.jpg?wait=0&attempt=0",
   },
-  {
-    title: "Joy Comes in the Morning",
-    id: 14,
-    category: "sunset butterfly monarch joy morning butterfly butterflies",
-    medium: "canvas wrap acrylic",
-    baseImage: joy,
-    size: "medium 16 x 20",
-    description: "Acrylic paint on 16x20 inch wrapped canvas.",
-    price: 200,
-    page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/joy.jpg",
-  },
+  // {
+  //   title: "Joy Comes in the Morning",
+  //   id: 14,
+  //   category: "sunset butterfly monarch joy morning butterfly butterflies",
+  //   medium: "canvas wrap acrylic",
+  //   baseImage: joy,
+  //   size: "medium 16 x 20",
+  //   description: "Acrylic paint on 16x20 inch wrapped canvas.",
+  //   price: 200,
+  //   page: "https://user-interface-ii-5fl9n5s96.vercel.app/images/art/joy.jpg",
+  // },
   {
     title: "Magnolias",
     id: 15,
