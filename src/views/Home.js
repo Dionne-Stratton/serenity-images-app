2a26917481066b9f2cee9aa761a08fdc16bf6d21
Dionne Stratton <mylife33@hotmail.com>
2021-05-30 02:16:43 -0500

    home page video

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,22 +1,49 @@
 import React from "react";
-import aboutArt from "../images/aboutArt.jpg";
+import aboutArt from "../images/serenityartadd.mp4";
 
 const Home = () => {
   return (
     <div className="homepage">
+      {/* <img src={aboutArt} alt="art" /> */}
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          // left: "50%",
+          // top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          // transform: "translate(-50%, -50%)",
+          zIndex: "-1",
+        }}
+      >
+        <source src={aboutArt} type="video/mp4" />
+      </video>
       <section className="section1">
-        <img src={aboutArt} alt="art" />
-        <div className="aboutbox">
-          <div className="textbox">
-            <h3>Why Fine Art?</h3>
+        <div>
+          <div>
+            <h4 className="vidover">Visit the Gallery for More</h4>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
             <br></br>
             <p>
               At Serenity Images Art we strive to create work that uplifts,
               encourages, and inspires. Art that leaves a smile on your face.
               Because life can be hard, but together we can thrive.
             </p>
-            <br></br>
-            <br></br>
           </div>
         </div>
       </section>
