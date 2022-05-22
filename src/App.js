67746ec23274356a79b093d8da0f5ec69bdcafd1
Dionne <mylife33@hotmail.com>
2022-05-21 20:30:41 -0500

    cleaned out unnecessary files and folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import HeaderNav from "./views/HeaderNav";
-import Footer from "./views/Footer";
+import HeaderNav from "./components/HeaderNav";
+import Footer from "./components/Footer";
 import Home from "./views/Home";
 import About from "./views/About";
-import Gallery from "./components/Gallery";
-import ProductPage from "./components/ProductPage";
-import Watch from "./components/Watch";
+import Gallery from "./views/Gallery";
+import ProductPage from "./views/ProductPage";
+import Watch from "./views/Watch";
 import axios from "axios";
 
 export default function App() {
