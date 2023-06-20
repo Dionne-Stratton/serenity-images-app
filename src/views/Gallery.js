5b2ae3c3d2035d213ef497b399baad020945eff8
Dionne <mylife33@hotmail.com>
2023-06-19 22:27:53 -0500

    Added a dropdown to filter by medium

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -5,6 +5,7 @@ export default function Gallery(props) {
   const { items } = props;
   const { url } = useRouteMatch();
   const [search, setSearch] = useState("");
+  const [medium, setMedium] = useState("all");
 
   const clearStyle = {
     textDecoration: "none",
@@ -19,12 +20,31 @@ export default function Gallery(props) {
         placeholder="Key Word Search"
         onChange={(e) => setSearch(e.target.value)}
       />
+      {/* // create a dropdown menu to filter the items by medium */}
+      <select
+        className="medium"
+        onChange={(e) => setMedium(e.target.value)}
+        value={medium}
+      >
+        <option value="all">All</option>
+        <option value="digital">Digital</option>
+        <option value="acrylic">Acrylic</option>
+        <option value="watercolor">Watercolor</option>
+        <option value="ink">Ink</option>
+      </select>
+
       {/* if the items array is empty, display the loading message */}
       {items.length === 0 && <p>Loading... Please wait...</p>}
       <div className="gallery">
         {items
           //we are filtering the items array to only display the items that have a keyword that includes the search variable. The search variable is set to the value of the input field as the user types. The filter method is case sensitive so we are converting the search variable to lowercase to ensure that the filter method works as expected
           .filter((art) => art.keyword.includes(search.toLowerCase()))
+          .filter((art) => {
+            if (art.medium === medium || medium === "all") {
+              return true;
+            }
+            return false;
+          })
           .map((item) => {
             const base64String = btoa(
               String.fromCharCode(...new Uint8Array(item.img.data.data))
